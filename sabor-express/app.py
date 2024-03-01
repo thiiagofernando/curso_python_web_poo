cdc27dc536484e69e5cb11da2b42df86a28cb568
Thiago Fernando <thiago.fernando@msn.com>
2024-02-29 21:04:25 -0400

    Modulo 1 Herança

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -4,11 +4,13 @@ from modelos.cardapio.prato import  Prato
 
 restaurante_praca = Restaurante('Praca','Gourmet')
 bebida_suco = Bebida('Suco Laranja',5.0,'Grande')
+prato_lasanha = Prato('Lasanha',15.0,'Com muito queijo')
 
 
 
 
 def main():
-    pass
+    print(bebida_suco)
+    print(prato_lasanha)
 if __name__ == '__main__':
     main()
